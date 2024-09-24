2bc5ae15c2e53a809756bdfad616f12e80ecf9d1
Kartikay Singh <kartikay.singh2023@vitstudent.ac.in>
2024-09-24 10:07:17 +0530

    ball and computer

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -46,7 +46,12 @@ const BallCanvas = ({ icon }) => {
 	return (
 		<Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
 			<Suspense fallback={<CanvasLoader />}>
-				<OrbitControls enableZoom={false} />
+				<OrbitControls
+					enableZoom={false}
+					maxPolarAngle={Math.PI / 2}
+					minPolarAngle={Math.PI / 2}
+					enablePan={false}
+				/>
 				<Ball imgUrl={icon} />
 			</Suspense>
 
